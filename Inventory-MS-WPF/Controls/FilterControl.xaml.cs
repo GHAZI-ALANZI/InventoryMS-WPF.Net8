abb85c5be0ab72323c8dfe86d811bfa6a33a3dc9
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2024-04-09 22:24:07 +0100

    create UserControllers for Orders,Revenue,Deliverd,Nav,Stock.....etc

---
+++ Inventory-MS-WPF/Controls/FilterControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Inventory_MS_WPF.Controls
+{
+    /// <summary>
+    /// Interaction logic for FilterControl.xaml
+    /// </summary>
+    public partial class FilterControl : UserControl
+    {
+
+        public string Filter
+        {
+            get { return (string)GetValue(FilterProperty); }
+            set { SetValue(FilterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Filter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilterProperty =
+            DependencyProperty.Register("Filter", typeof(string), typeof(FilterControl), new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
+        public FilterControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
